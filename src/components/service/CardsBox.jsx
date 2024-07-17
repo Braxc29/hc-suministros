dfd3abf92e0769a14ed4d542131162c139001fee
Gottbow <garyprado45@gmail.com>
2024-07-16 22:35:48 -0500

    servicios

--- src/components/service/CardsBox.jsx
+++ src/components/service/CardsBox.jsx
@@ -4,7 +4,8 @@ export const CardBox = ({ blog }) => {
 	return (
 		<div className='h-full w-[400px] m-2 flex-shrink-0 cursor-pointer'>
 			<div className='rounded-3xl overflow-hidden mb-4 relative h-[250px]'>
-				<img src={`/src/assets/images/${blog.id}.jpg`} alt={blog.title} />
+				{/* <
+				 src={`/src/assets/images/${blog.id}.jpg`} alt={blog.title} /> */}
 			</div>
 			<div className='px-4 flex gap-4'>
 				<div className='flex flex-col gap-2 w-full'>
